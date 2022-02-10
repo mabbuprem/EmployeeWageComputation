cfbeb82cf5f69c638960e31e5682cac00c87299c
mabbuprem <mabbupremkumar@gmail.com>
2022-02-10 21:09:18 +0530

    EmployeeDailyWage

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeWageComputation
+namespace EmployeeWageProblem
 {
-    internal class UC1_EmployeeAttendence
+    internal class UC2_EmployeeDailyWage
     {
         public static void GetAttendence()
         {
@@ -17,9 +15,32 @@ namespace EmployeeWageComputation
             Random random = new Random();
             int empcheck = random.Next(0.2);
             if (empcheck == IS_FULL_TIME)
+
                 Console.WriteLine("Employee is present");
             else
                 Console.WriteLine("Employee is absent");
         }
+
+        public static void EmpWage()
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                empHrs = 8;
+            //else
+            // empHrs = 0;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: + empWage");
+            Console.ReadKey();
+        }
+
     }
+
 }
+    
+
