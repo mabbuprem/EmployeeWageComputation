d2352958c7ce9da1697a8f4bedba81303f576a04
mabbuprem <mabbupremkumar@gmail.com>
2022-02-10 23:46:09 +0530

    empWageUsingSwitch

--- Program.cs
+++ Program.cs
@@ -5,43 +5,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeWageCalculator
+namespace EmployeeWageProblem
 {
-    class Program
+    internal class EmpWageUsingSwitch
     {
-        public static void Main(string[] args)
-
+        public static void GetAttendence()
         {
-            Console.WriteLine($"Employee Wage Calculator");
-            int workingHr = 1;
-            int perHrSalary = 20;
-
+            int IS_FULL_TIME = 1;
             Random random = new Random();
-            int present = (random.Next(2));
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        }
 
-            switch (present)
+        public static void EmpWage()
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+                //variable
+                int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                empHrs = 8;
+                //else
+                //emHrs = 0;
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
+            Console.ReadKey();
+
+        }
+
+        public static void partTimeEmployeeWage()
+        {
+            //constant
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            //variable
+            int empHrs = 0;
+            int empWage = 0;
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 2);
+            if (empCheck == IS_PART_TIME)
+                empHrs = 4;
+            else if (empCheck == IS_FULL_TIME)
+                empHrs = 8;
+            else
+                empHrs = 0;
+            empWage = empHrs *
+        EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: + empWage");
+            Console.ReadKey();
+
+
+        }
+        internal class EmployeeWage
+        {
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 0;
+            const int IS_PRESENT = 1;
+            const int IS_ABSENT = 0;
+            const int EMP_PER_HOUR = 20;
+
+
+            public void GetDailyWage()
             {
+                int dailyWage = 0;
+                int dailyHours = 0;
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        dailyHours = 8;
+                        break;
+                    case IS_PART_TIME:
+                        dailyHours = 4;
+                        break;
+                    default:
+                        dailyHours = 0;
+                        break;
 
-                case 0:
-                    Console.WriteLine("Employee is absent");
-                    workingHr = 0;
-                    break;
-
-                case 1:
-                    Console.WriteLine("Employee is present");
-                    workingHr = 8;
-                    break;
-                default:
-                    Console.WriteLine("error");
-                    break;
+                }
+                dailyWage = dailyHours * RATE_PER_HOUR;
+                Console.WriteLine("Daily Wage: " + dailyWage);
             }
-
-            int salary = perHrSalary * workingHr;
-            Console.WriteLine($"Salary {salary}");
-
         }
     }
 }
-
+      
     
 
