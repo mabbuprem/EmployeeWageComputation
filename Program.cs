4c5ca6905a7da0b54c815d26aaf62698e5387c1e
mabbuprem <mabbupremkumar@gmail.com>
2022-02-15 20:56:24 +0530

    maxhrs maxdays

--- Program.cs
+++ Program.cs
@@ -5,98 +5,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeWageProblem
+namespace EmployeeWage
 {
-    internal class EmpWageUsingSwitch
+    internal class EmployeeWage
     {
-        public static void GetAttendence()
+        static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("Employee is present");
-            else
-                Console.WriteLine("Employee is absent");
-        }
+            const int ISPRESENT = 1;
+            const int ISPARTTIME = 2;
+            const int ISABSENT = 0;
+            const int WAGEPERHOUR = 20;
+            const int TOTALWORKINGDAYS = 20;
+            const int MAXHRSWORKED = 100;
 
-        public static void EmpWage()
-        {
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
-                //variable
-                int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
-                empHrs = 8;
-                //else
-                //emHrs = 0;
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage: " + empWage);
-            Console.ReadKey();
+            int workingHours = 8;
+            int totalWorkingHrs = 0;
+            int workingDays = 0;
 
-        }
-
-        public static void partTimeEmployeeWage()
-        {
-            //constant
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-            //variable
-            int empHrs = 0;
-            int empWage = 0;
-            Random rand = new Random();
-            int empCheck = rand.Next(0, 2);
-            if (empCheck == IS_PART_TIME)
-                empHrs = 4;
-            else if (empCheck == IS_FULL_TIME)
-                empHrs = 8;
-            else
-                empHrs = 0;
-            empWage = empHrs *
-        EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage: + empWage");
-            Console.ReadKey();
-
-
-        }
-        internal class EmployeeWage
-        {
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 0;
-            const int IS_PRESENT = 1;
-            const int IS_ABSENT = 0;
-            const int EMP_PER_HOUR = 20;
-
-
-            public void GetDailyWage()
+            while (workingDays < TOTALWORKINGDAYS && totalWorkingHrs < MAXHRSWORKED)
             {
-                int dailyWage = 0;
-                int dailyHours = 0;
                 Random random = new Random();
-                int empCheck = random.Next(0, 2);
-                switch (empCheck)
+                int employeeCheck = random.Next(0, 3);
+                switch (employeeCheck)
                 {
-                    case IS_FULL_TIME:
-                        dailyHours = 8;
+                    case ISPRESENT:
+                        //Console.WriteLine("Employee is present");
+                        workingHours = 8;
                         break;
-                    case IS_PART_TIME:
-                        dailyHours = 4;
+                    case ISPARTTIME:
+                        //Console.WriteLine("Employee is part-time working");
+                        workingHours = 8;
                         break;
                     default:
-                        dailyHours = 0;
+                        //Console.WriteLine("Employee is absent");
+                        workingHours = 0;
                         break;
-
                 }
-                dailyWage = dailyHours * RATE_PER_HOUR;
-                Console.WriteLine("Daily Wage: " + dailyWage);
+                totalWorkingHrs = totalWorkingHrs + workingHours;
+                workingDays++;
             }
+            int employeeWage = totalWorkingHrs * WAGEPERHOUR;
+            Console.WriteLine($"Employee earns {employeeWage}");
+            Console.ReadLine();
+
         }
     }
 }
-      
-    
+
+
 
