74b67b1a7ce2c644882d4c631fe9eb25bb1821cd
mabbuprem <mabbupremkumar@gmail.com>
2022-02-15 21:53:44 +0530

    daily wage and total wage

--- Program.cs
+++ Program.cs
@@ -15,26 +15,26 @@ namespace EmployeeWage
             const int ISPARTTIME = 2;
             const int ISABSENT = 0;
 
-            int workingHours = 8;
+            int workDayHours = 8;
 
             Random random = new Random();
             int employeeCheck = random.Next(0, 3);
             switch (employeeCheck)
             {
                 case ISPRESENT:
-                    //Console.WriteLine("Employee is present");
-                    workingHours = 8;
-                    return workingHours;
+                    Console.WriteLine("Employee is present");
+                    workDayHours = 8;
+                    return workDayHours;
                     break;
                 case ISPARTTIME:
-                    //Console.WriteLine("Employee is part-time working");
-                    workingHours = 8;
-                    return workingHours;
+                    Console.WriteLine("Employee is part-time working");
+                    workDayHours = 8;
+                    return workDayHours;
                     break;
                 default:
-                    //Console.WriteLine("Employee is absent");
-                    workingHours = 0;
-                    return workingHours;
+                    Console.WriteLine("Employee is absent");
+                    workDayHours = 0;
+                    return workDayHours;
                     break;
             }
         }
@@ -45,16 +45,27 @@ namespace EmployeeWage
             const int MAXHRSWORKED = 100;
 
 
+            int workingHrs = 0;
             int totalWorkingHrs = 0;
             int workingDays = 0;
+            int totalWage = 0;
+            int[] dailyWageArray = new int[20];
+            int[] totalWageArray = new int[20];
 
             while (workingDays < TOTALWORKINGDAYS && totalWorkingHrs < MAXHRSWORKED)
             {
-                totalWorkingHrs = totalWorkingHrs + getworkhrs();
+                workingHrs = getworkhrs();
+                dailyWageArray[workingDays] = WAGEPERHOUR * workingHrs;
+                totalWage = totalWage + WAGEPERHOUR * workingHrs;
+                totalWageArray[workingDays] = totalWage;
+                totalWorkingHrs = totalWorkingHrs + workingHrs;
                 workingDays++;
+
             }
             int employeeWage = totalWorkingHrs * WAGEPERHOUR;
             Console.WriteLine($"Employee earns {employeeWage}");
+            Console.WriteLine(String.Join(" ", dailyWageArray));
+            Console.WriteLine(String.Join(" ", totalWageArray));
             Console.ReadLine();
 
         }
