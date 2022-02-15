e4aff9618a46faf18db960593ded38d0eddd104a
mabbuprem <mabbupremkumar@gmail.com>
2022-02-15 21:17:54 +0530

    get work hrs function

--- Program.cs
+++ Program.cs
@@ -9,39 +9,48 @@ namespace EmployeeWage
 {
     internal class EmployeeWage
     {
-        static void Main(string[] args)
+        static int getworkhrs()
         {
             const int ISPRESENT = 1;
             const int ISPARTTIME = 2;
             const int ISABSENT = 0;
+
+            int workingHours = 8;
+
+            Random random = new Random();
+            int employeeCheck = random.Next(0, 3);
+            switch (employeeCheck)
+            {
+                case ISPRESENT:
+                    //Console.WriteLine("Employee is present");
+                    workingHours = 8;
+                    return workingHours;
+                    break;
+                case ISPARTTIME:
+                    //Console.WriteLine("Employee is part-time working");
+                    workingHours = 8;
+                    return workingHours;
+                    break;
+                default:
+                    //Console.WriteLine("Employee is absent");
+                    workingHours = 0;
+                    return workingHours;
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
             const int WAGEPERHOUR = 20;
             const int TOTALWORKINGDAYS = 20;
             const int MAXHRSWORKED = 100;
 
-            int workingHours = 8;
+
             int totalWorkingHrs = 0;
             int workingDays = 0;
 
             while (workingDays < TOTALWORKINGDAYS && totalWorkingHrs < MAXHRSWORKED)
             {
-                Random random = new Random();
-                int employeeCheck = random.Next(0, 3);
-                switch (employeeCheck)
-                {
-                    case ISPRESENT:
-                        //Console.WriteLine("Employee is present");
-                        workingHours = 8;
-                        break;
-                    case ISPARTTIME:
-                        //Console.WriteLine("Employee is part-time working");
-                        workingHours = 8;
-                        break;
-                    default:
-                        //Console.WriteLine("Employee is absent");
-                        workingHours = 0;
-                        break;
-                }
-                totalWorkingHrs = totalWorkingHrs + workingHours;
+                totalWorkingHrs = totalWorkingHrs + getworkhrs();
                 workingDays++;
             }
             int employeeWage = totalWorkingHrs * WAGEPERHOUR;
