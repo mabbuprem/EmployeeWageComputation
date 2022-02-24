67d6101eb6357ef871d672d7dca8e2cf70abf992
mabbuprem <mabbupremkumar@gmail.com>
2022-02-24 14:27:45 +0530

    get employee wage

---
+++ GetEmployeeWage.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wageCalculator
+{
+    internal class GetEmployeeWage
+    {
+
+        public string CompanyName;
+        public int EmpRatePerHr;
+        public int MaxDays;
+        public int MaxHrs;
+
+        public GetEmployeeWage(string companyName, int empRatePerHr, int maxDays, int maxHrs)
+        {
+            CompanyName = companyName;
+            EmpRatePerHr = empRatePerHr;
+            MaxDays = maxDays;
+            MaxHrs = maxHrs;
+            EmployeeWage();
+        }
+
+        public void EmployeeWage()
+        {
+            //declaration and initilization
+            int totalSalary = 0;
+            int totalHr = 0;
+            Dictionary<int, int> dailyWageDict = new Dictionary<int, int>();
+
+            for (int day = 0; day < MaxDays; day++)
+            {
+
+
+                int workingHr = GetWorkingHr();
+                totalHr = totalHr + workingHr;
+                if (totalHr >= MaxHrs)
+                {
+                    break;
+                }
+                int salary = EmpRatePerHr * workingHr;
+
+                totalSalary = totalSalary + salary;
+                dailyWageDict.Add(day, salary);
+
+            }
+            Console.WriteLine(CompanyName);
+            Console.WriteLine($"Total Salary: {totalSalary}");
+            Console.WriteLine($"Total working hours: {totalHr}");
+            //Console.WriteLine(String.Join(" ", dailyWageArray));
+
+            for (int i = 0; i < dailyWageDict.Count; i++)
+            {
+                Console.WriteLine($"day {i + 1}  wage {dailyWageDict[i]}");
+
+            }
+        }
+
+
+        public int GetWorkingHr()
+        {
+            //constant
+            const int ABSENT = 0;
+            const int PRESENT = 1;
+            const int PART_TIME = 2;
+            //declaration and initilization
+            int workingHr = 0;
+
+            Random rnd = new Random();
+            int present = (rnd.Next(3));
+            switch (present)
+            {
+
+                case ABSENT:
+                    workingHr = 0;
+                    break;
+                case PRESENT:
+                    workingHr = 8;
+                    break;
+                case PART_TIME:
+                    workingHr = 4;
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+            return workingHr;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,84 +1,20 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EmployeeWage
+namespace wageCalculator;
+
+class Program
 {
-    internal class EmployeeWage
+    public static void Main(String[] args)
+
     {
-        static int getworkhrs()
-        {
-            //CONSTANTS
-            const int ISPRESENT = 1;
-            const int ISPARTTIME = 2;
-            const int ISABSENT = 0;
-            //VARIABLES
-            int workDayHours = 0;
+        Console.WriteLine($"Employee Wage Calculator");
+        GetEmployeeWage Dmart = new GetEmployeeWage("Dmart", 20, 20, 100);
+        GetEmployeeWage Reliance = new GetEmployeeWage("Reliance", 25, 22, 50);
 
-            Random random = new Random();
-            int employeeCheck = random.Next(0, 3);
-            switch (employeeCheck)
-            {
-                case ISPRESENT:
-                    Console.WriteLine("Employee is present");
-                    workDayHours = 8;
-                    return workDayHours;
-                    break;
-                case ISPARTTIME:
-                    Console.WriteLine("Employee is part-time working");
-                    workDayHours = 4;
-                    //I have changed the workDayHours for part time working to 4 hrs.
-                    return workDayHours;
-                    break;
-                default:
-                    Console.WriteLine("Employee is absent");
-                    workDayHours = 0;
-                    return workDayHours;
-                    break;
-            }
-        }
-        static void Main(string[] args)
-        {
-            //CONSTANTS
-            const int WAGEPERHOUR = 20;
-            const int TOTALWORKINGDAYS = 20;
-            const int MAXHRSWORKED = 100;
-
-            //VARIABLES
-            int workingHrs = 0;
-            int totalWorkingHrs = 0;
-            int workingDays = 1;
-            int totalWage = 0;
-            int i = 0;
-            int[] workingDaysArray = new int[20];
-            int[] dailyWageArray = new int[20];
-            int[] totalWageArray = new int[20];
-
-            while (workingDays <= TOTALWORKINGDAYS && totalWorkingHrs < MAXHRSWORKED)
-            {
-                workingHrs = getworkhrs();
-                workingDaysArray[i] = workingDays;
-                dailyWageArray[i] = WAGEPERHOUR * workingHrs;
-                totalWage = totalWage + WAGEPERHOUR * workingHrs;
-                totalWageArray[i] = totalWage;
-                totalWorkingHrs = totalWorkingHrs + workingHrs;
-                workingDays++;
-                i++;
-
-            }
-            int employeeWage = totalWorkingHrs * WAGEPERHOUR;
-            Console.WriteLine($"Employee earns {employeeWage}");
-            Console.WriteLine(String.Join(" ", workingDaysArray));
-            Console.WriteLine(String.Join(" ", dailyWageArray));
-            Console.WriteLine(String.Join(" ", totalWageArray));
-            Console.ReadLine();
-
-        }
     }
 }
+
 
 
 
